1602d6a83bf3b9549e829f36fd440a5a7e7a04f9
Natã Trindade da Silva <110295585+trindade-010@users.noreply.github.com>
2022-08-24 11:02:11 -0300

    criei ambiente.js

---
+++ aula08/ambiente.js
@@ -0,0 +1,18 @@
+/*var c = 1 
+
+while (c <= 10) {
+    console.log(`Passo ${c}`)
+    c++
+}*/
+//-------------------------------------------
+/*var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 10)*/
+//-------------------------------------------
+console.log('Vai começar...')
+for(var c = 1; c <= 4; c++ ){
+    console.log(`Passo ${c}`)
+}
+console.log('FIM!')
