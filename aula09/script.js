b9870df8bca8be7370e97d205f1a3580b14968e6
Natã Trindade da Silva <110295585+trindade-010@users.noreply.github.com>
2022-08-25 14:33:27 -0300

    Terminei a aula09

--- aula09/script.js
+++ aula09/script.js
@@ -2,9 +2,29 @@ function contar() {
     var ini = document.getElementById('txti')
     var fim = document.getElementById('txtf')
     var pass = document.getElementById('txtp')
+    let res = document.querySelector('div#res')
    if(ini.value.length == 0 || fim.value.length == 0 || pass.value.length == 0){
-        window.alert('[ERRO] Faltam dados!')
+        res.innerHTML = 'Impossivel Contar !'
+           window.alert('[ERRO] Faltam dados!')
+        
    } else { 
-    alert('Tudo ok')
+     res.innerHTML = 'Contando: <br>'
+     let i = Number(ini.value)
+     let f = Number(fim.value)
+     let p = Number(pass.value)
+     if (p <= 0 ){
+          window.alert('Passo inválido! Considerando Passa 1')
+     }
+     if (i < f){ //Contagem crescente
+          for(let c = i ; c <= f ; c += p ){
+          res.innerHTML += ` ${c} \u{1F449}`
+     }
+      } else {
+          //Contagem regressiva
+          for (let c = i; c >= f; c-= p){ 
+               res.innerHTML += ` ${c} \u{1F449}`
+          }
+     }
+       res.innerHTML += `\u{1F3C1}`
    }
 }
